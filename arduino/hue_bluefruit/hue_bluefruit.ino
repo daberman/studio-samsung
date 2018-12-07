b74d224dbcc7f04e51c6e6410c08beb9643a4eda
Dan Berman <dan.megrue.berman@gmail.com>
2018-12-07 12:18:13 -0500

    Updated hue_bluefruit.ino to handle commands received over bluetooth uart. Untested

--- arduino/hue_bluefruit/hue_bluefruit.ino
+++ arduino/hue_bluefruit/hue_bluefruit.ino
@@ -1,33 +1,33 @@
 /*
- * hue_bluefuit.ino
- * by Dan Berman
- * created 12/4/18
- * updated 12/4/18
- * 
- * Arduino code for CT Product Studio - team 75 (Samsung). Controls the mock Philips Hue lightbulb.
- * Requires the Adafruit_NeoPixel library. An optional RGB LED can be connected to the board to use
- * as a startup status indicator.
- * 
- * This code is written to be used on an Adafruit nRF52 Bluefruit Feather board 
- * (https://www.adafruit.com/product/3406)
- * 
- * User guide located at: https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/introduction
- * GitHub Repo: https://github.com/adafruit/Adafruit_nRF52_Arduino
- * 
- * BSP Installation (necessary):
- *  - Add https://www.adafruit.com/package_adafruit_index.json as an 'Additional Board Manager URL'
- *    (Arduino -> Preferences -> Settings)
- *  - Install 'Adafruit nRF52 by Adafruit' from Tools -> Board -> Boards Manager
- *  - Select Tools -> Board -> "Adafruit Bluefruit nRF52832 Feather"
- *  - Ensure Tools -> Programmer is set to "Bootloader DFU for Bluefruit nRF52"
- *  
- * CP2104 Driver (necessary if not previously installed for a different board):
- *   - https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers
- *   - Select Tools -> Port -> SLAB_USBtoUART (or similar)
- *   
- * Bootloader Update (try if getting a timeout error when uploading to board):
- *  - Tools -> Burn Bootloader
- */
+   hue_bluefuit.ino
+   by Dan Berman
+   created 12/4/18
+   updated 12/4/18
+
+   Arduino code for CT Product Studio - team 75 (Samsung). Controls the mock Philips Hue lightbulb.
+   Requires the Adafruit_NeoPixel library. An optional RGB LED can be connected to the board to use
+   as a startup status indicator.
+
+   This code is written to be used on an Adafruit nRF52 Bluefruit Feather board
+   (https://www.adafruit.com/product/3406)
+
+   User guide located at: https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/introduction
+   GitHub Repo: https://github.com/adafruit/Adafruit_nRF52_Arduino
+
+   BSP Installation (necessary):
+    - Add https://www.adafruit.com/package_adafruit_index.json as an 'Additional Board Manager URL'
+      (Arduino -> Preferences -> Settings)
+    - Install 'Adafruit nRF52 by Adafruit' from Tools -> Board -> Boards Manager
+    - Select Tools -> Board -> "Adafruit Bluefruit nRF52832 Feather"
+    - Ensure Tools -> Programmer is set to "Bootloader DFU for Bluefruit nRF52"
+
+   CP2104 Driver (necessary if not previously installed for a different board):
+     - https://www.silabs.com/products/development-tools/software/usb-to-uart-bridge-vcp-drivers
+     - Select Tools -> Port -> SLAB_USBtoUART (or similar)
+
+   Bootloader Update (try if getting a timeout error when uploading to board):
+    - Tools -> Burn Bootloader
+*/
 
 #include <bluefruit.h>
 #include <Adafruit_NeoPixel.h>
@@ -36,8 +36,8 @@
 /* Constants */
 
 // DEBUG to enable Serial prints over USB @ 115200 baud
-#define DEBUG  true // Enable debug prints
-#define DEBUG2 false // Add delays during setup
+#define DEBUG 1 // Enable debug prints
+
 
 // Constants for the RGB status LED
 #define STATUS_LED_EN     true
@@ -63,6 +63,7 @@
 Adafruit_NeoPixel bulb = Adafruit_NeoPixel(BULB_PIXELS, BULB_PIN, NEO_GRB + NEO_KHZ800);
 
 bool on;
+bool enRainbow;
 int brightness;
 int red;
 int green;
@@ -84,13 +85,13 @@ void setup()
     Serial.println();
   }
 
-  if ( DEBUG2 ) delay(1000);
-  
+  if ( DEBUG > 1 ) delay(1000);
+
   // Setup status LED
   if ( STATUS_LED_EN )
   {
     if ( DEBUG ) Serial.println("Status LED enabled. Initializing...");
-    
+
     pinMode(STATUS_R_LED_PIN, OUTPUT);
     pinMode(STATUS_G_LED_PIN, OUTPUT);
     pinMode(STATUS_B_LED_PIN, OUTPUT);
@@ -99,25 +100,26 @@ void setup()
     digitalWrite(STATUS_B_LED_PIN, LOW);
   }
 
-  if ( DEBUG2 ) delay(1000);
+  if ( DEBUG > 1 ) delay(1000);
 
   // Setup NeoPixels for the bulb
   if ( DEBUG ) Serial.println("Initializing NeoPixels...");
   if ( STATUS_LED_EN ) digitalWrite(STATUS_R_LED_PIN, HIGH); // Turn on red
-  
+
   red = 255;
   green = 255;
   blue = 255;
   brightness = 100;
   on = false;
+  enRainbow = false;
 
   bulb.begin();
   setColor();
   bulb.show();
 
-  if ( DEBUG2 ) delay(1000);
+  if ( DEBUG > 1) delay(1000);
 
-  // Init Bluefruit 
+  // Init Bluefruit
   if ( DEBUG ) Serial.println("Initializing Bluetooth...");
   if ( STATUS_LED_EN ) digitalWrite(STATUS_B_LED_PIN, HIGH); // Add blue to make purple!
 
@@ -128,6 +130,7 @@ void setup()
   Bluefruit.setConnectCallback(connect_callback);
   Bluefruit.setDisconnectCallback(disconnect_callback);
 
+  bleuart.setRxCallback(bleuart_callback);
   bleuart.begin(); // Configure and start the BLE Uart service
 
   startAdv(); // Set up and start advertising
@@ -139,84 +142,98 @@ void setup()
 /* Main loop */
 void loop()
 {
-  
-  if ( cmdRcvd() )
-  {
-    char cmd = cmdRead();
-    char c;
-
-    if ( DEBUG ) Serial.println("Processing command");
-
-    switch (cmd) {
-
-      case 'b': // Update brightness
-        brightness = Serial.parseInt();
-
-        setColor();
-
-        Serial.flush(); // Clear the buffer
-        break;
-
-      case 'c': // Update color
-        red = Serial.parseInt();
-        green = Serial.parseInt();
-        blue = Serial.parseInt();
-
-        setColor();
-
-        Serial.flush(); // Clear the buffer
-        break;
-
-      case 'o': // Set On/Off
-        if ( DEBUG ) Serial.println("On/Off command received");
-        c = Serial.read();
-        if (c == 'n') { on = true; Serial.println("Turning on"); }
-        else if (c == 'f') { on = false; Serial.println("Turning off"); }
-        setColor();
-
-        Serial.flush(); // Clear the buffer
-        break;
-
-      case 'r': // Turn on Rainbow mode (send 'r' to turn off)
-        Serial.flush(); // Clear the buffer
-        
-        rainbowScroll(5);        
-        break;
-
-      default:
-        break;
-    }
-  }
-  
+  if (DEBUG && Serial.available()) processCmd(&Serial);
+  if (on && enRainbow) rainbowScroll(10);
 }
+
+/* Process a command from a Stream */
+void processCmd(Stream *s)
+{
+  char cmd = s->read();
+  char c;
+
+  if ( DEBUG ) Serial.println("Processing command");
+
+  switch (cmd) {
+
+    case 'b': // Update brightness
+      if (DEBUG > 2) Serial.println("Updating brightness");
+      brightness = s->parseInt();
+
+      setColor();
+
+      s->flush(); // Clear the buffer
+      break;
+
+    case 'c': // Update color
+      if (DEBUG > 2) Serial.println("Updating color");
+      red = s->parseInt();
+      green = s->parseInt();
+      blue = s->parseInt();
+
+      setColor();
+
+      s->flush(); // Clear the buffer
+      break;
+
+    case 'o': // Set On/Off
+      if (DEBUG > 2) Serial.println("On/Off command received");
+      c = s->read();
+      if (c == 'n') {
+        on = true;
+      }
+      else if (c == 'f') {
+        on = false;
+      }
+      setColor();
+
+      s->flush(); // Clear the buffer
+      break;
+
+    case 'r': // Turn on Rainbow mode (send 'r' to turn off)
+      if (DEBUG > 2) Serial.println("Rainbow command received");
+      s->flush(); // Clear the buffer
+
+      enRainbow = !enRainbow;
+      break;
+
+    case 'P': // Debug print what is being sent over bluetooth to Serial
+      if (DEBUG > 2) Serial.print("Received via bluetooth: ");
+      if ( DEBUG ) debugBLEUart();
+      break;
+
+    default:
+      break;
+  } // End switch
+} // End processCmd()
 
 /* Bluefruit Functions */
 void startAdv(void)
-{  
+{
   // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-  
+
   // Include bleuart 128-bit uuid
   Bluefruit.Advertising.addService(bleuart);
 
   // Secondary Scan Response packet (optional)
   // Since there is no room for 'Name' in Advertising packet
   Bluefruit.ScanResponse.addName();
-  
+
   /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
+     - Enable auto advertising if disconnected
+     - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+     - Timeout for fast mode is 30 seconds
+     - Start(timeout) with timeout = 0 will advertise forever (until connected)
+
+     For recommended advertising interval
+     https://developer.apple.com/library/content/qa/qa1931/_index.html
+  */
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
 
 void connect_callback(uint16_t conn_handle)
@@ -224,13 +241,13 @@ void connect_callback(uint16_t conn_handle)
   char central_name[32] = { 0 };
   Bluefruit.Gap.getPeerName(conn_handle, central_name, sizeof(central_name));
 
-  if ( DEBUG ) 
-  { 
-    Serial.print("Connected to "); 
-    Serial.println(central_name); 
+  if ( DEBUG )
+  {
+    Serial.print("Connected to ");
+    Serial.println(central_name);
   }
-  if ( STATUS_LED_EN ) 
-  { 
+  if ( STATUS_LED_EN )
+  {
     digitalWrite(STATUS_R_LED_PIN, LOW);
     digitalWrite(STATUS_G_LED_PIN, HIGH);
     digitalWrite(STATUS_B_LED_PIN, LOW);
@@ -240,11 +257,27 @@ void connect_callback(uint16_t conn_handle)
 void disconnect_callback(uint16_t conn_handle, uint8_t reason)
 {
   if ( DEBUG ) Serial.println("Bluetooth disconnected");
-  if ( STATUS_LED_EN ) 
+  if ( STATUS_LED_EN )
   {
     digitalWrite(STATUS_R_LED_PIN, LOW);
     digitalWrite(STATUS_G_LED_PIN, LOW);
     digitalWrite(STATUS_B_LED_PIN, HIGH);
+  }
+}
+
+void bleuart_callback()
+{
+  processCmd(&bleuart);
+}
+
+void debugBLEUart()
+{
+  while (bleuart.available())
+  {
+    delay(2);
+    uint8_t buf[64];
+    int count = bleuart.readBytes(buf, sizeof(buf));
+    Serial.write(buf, count);
   }
 }
 
@@ -256,7 +289,7 @@ void setColor()
   int r = 0;
   int g = 0;
   int b = 0;
-  
+
   if (on)
   {
     // Adjust color levels by brightness
@@ -278,45 +311,30 @@ void rainbowScroll(uint8_t wait) {
   uint32_t wheelVal;
   int redVal, greenVal, blueVal;
 
-  bool rainbow = true;
-
-  while (rainbow)
+  while (on && enRainbow)
   {
 
     for (int j = 0; j < 256; j++) // 5 cycles of all colors on wheel
-    { 
+    {
 
       wheelVal = Wheel(j & 255);
 
-      redVal = redT(wheelVal);
-      greenVal = greenT(wheelVal);
-      blueVal = blueT(wheelVal);
+      redVal = redT(wheelVal) * brightness/100;
+      greenVal = greenT(wheelVal) * brightness/100;
+      blueVal = blueT(wheelVal) * brightness/100;
 
-      if (on)
+      for (int i = 0; i < bulb.numPixels(); i++)
       {
-        for (int i = 0; i < bulb.numPixels(); i++)
-        {
-          bulb.setPixelColor( i, bulb.Color( redVal, greenVal, blueVal ) );
-        }
+        bulb.setPixelColor( i, bulb.Color( redVal, greenVal, blueVal ) );
       }
 
       bulb.show();
       delay(wait);
 
-      if (Serial.available() > 0)
-      {
-        char cmd = Serial.read();
-        if (cmd == 'r') // Receive an 'r' to turn off Rainbow mode
-        {
-          rainbow = false;
-          break;
-        }
-      }
-    }
+    } // End for
 
-  }
-
-}
+  } // End while(rainbow)
+} // end rainbowScroll()
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
@@ -341,29 +359,4 @@ uint8_t greenT(uint32_t c) {
 }
 uint8_t blueT(uint32_t c) {
   return (c);
-}
-
-/* Command Helper functions */
-
-bool cmdRcvd()
-{
-  return bleuart.available() > 0 || ( DEBUG && Serial.available() > 0);
-}
-
-char cmdRead()
-{
-  if ( DEBUG && Serial.available() > 0 ) return Serial.read();
-  else return bleuart.read();
-}
-
-char cmdParseInt()
-{
-  if ( DEBUG && Serial.available() > 0 ) return Serial.parseInt();
-  else return bleuart.parseInt();
-}
-
-void cmdFlush()
-{
-  if ( DEBUG && Serial.available() > 0 ) Serial.flush();
-  bleuart.flush();
 }
