a15e96fd5c8c88b4bc7b842191069f1b8420e4ba
Dan Berman <dan.megrue.berman@gmail.com>
2018-12-07 12:25:10 -0500

    Initial commit of iOS app

---
+++ ios/SmartTable/SmartTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartTable
+//
+//  Created by Dan Berman on 12/7/18.
+//  Copyright © 2018 Dan Berman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
