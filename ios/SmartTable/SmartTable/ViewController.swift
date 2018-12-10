34b04fc4d474e96953821bf9d5ab48b023f7c4ba
Dan Berman <dan.megrue.berman@gmail.com>
2018-12-10 17:14:13 -0500

    Added CBCentralManager and scanning functionality to populate table of available BLE peripherals upon pressing the 'Refresh' button.

--- ios/SmartTable/SmartTable/ViewController.swift
+++ ios/SmartTable/SmartTable/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CBCentralManagerDelegate {
     
     // MARK: Properties
     
@@ -18,16 +18,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var refreshBTButton: UIButton!
     @IBOutlet weak var bulbPowerSwitch: UISwitch!
     
-    var availableBT = [CBPeripheral]()
+    let SCAN_TIMEOUT = 5.0
     
-    // MARK: viewDidLoad
+    var availableBT = [CBPeripheral]()
+    var timer = Timer()
+    
+    var btManager : CBCentralManager!
+    
+    // MARK: ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         btTableView.delegate = self
+        btTableView.dataSource = self
+        
         bulbPowerSwitch.isOn = false
+        
         btStatusLabel.text = "Please refresh list"
+        
+        btManager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey : true])
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cancelScan()
     }
     
     // MARK: TableViewDelegate
@@ -57,13 +71,64 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
+    
+    // MARK: CBCentralManagerDelegate
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        // nothing to do here since scanning is controlled by button action
+    }
+    
+    // Called when a peripheral is discovered while scanning
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        if peripheral.name != nil { // Check periphal has a name
+            // Confirm peripheral isn't already in our discovered list
+            var newPeripheral = true
+            for p in availableBT {
+                if p.identifier == peripheral.identifier {
+                    newPeripheral = false
+                    break
+                }
+            }
+            // Add peripheral to list of available peripherals if it is new
+            if newPeripheral {
+                self.availableBT.append(peripheral)
+                self.btTableView.reloadData()
+                print("Found new pheripheral devices with services")
+                print("Peripheral name: \(String(describing: peripheral.name))")
+                print("**********************************")
+                print("Advertisement Data : \(advertisementData)")
+                print("UUID: \(peripheral.identifier)\n")
+            }
+        }
+    }
 
     // MARK: Actions
     
     @IBAction func refreshBTPressed(_ sender: Any) {
+        availableBT = []
+        startScan()
     }
     
     @IBAction func bulbPowerToggled(_ sender: Any) {
+    }
+    
+    // MARK: Private functions
+    
+    private func startScan() {
+        print("Now Scanning...")
+        btStatusLabel.text = "Scanning"
+        self.timer.invalidate()
+        btManager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey : false])
+        Timer.scheduledTimer(timeInterval: SCAN_TIMEOUT, target: self, selector: #selector(self.cancelScan),
+                             userInfo: nil, repeats: false)
+    }
+    
+    @objc private func cancelScan() {
+        self.btManager?.stopScan()
+        self.timer.invalidate()
+        print("Scan Stopped")
+        btStatusLabel.text = "Scan stopped"
     }
     
 }
